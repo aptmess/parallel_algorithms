b59f3ffe6453eabd93d6e2eecdfd41df3f5c9dae
aptmess <improfeo@yandex.ru>
2021-12-17 16:24:21 +0300

    refactor task_3

--- HT/hw_openmp/prog/3.cpp
+++ HT/hw_openmp/prog/3.cpp
@@ -7,16 +7,16 @@
 using namespace std;
 
 // The function of allocating memory for a two-dimensional array
-double** malloc_array(long int n)
+int** malloc_array(long int n)
 {
-	double** matrix = new double*[n];
+	int** matrix = new int*[n];
 	for (int i = 0; i < n; i++)
-		matrix[i] = new double[n];
+		matrix[i] = new int[n];
 	return matrix;
 }
 
 // Memory free function
-void free_array(double** matrix,long int n)
+void free_array(int** matrix,long int n)
 {
 	for (int i = 0; i < n; i++)
 		delete[] matrix[i];
@@ -24,16 +24,15 @@ void free_array(double** matrix,long int n)
 }
 
 // The function of initializing a matrix with random numbers from [0,1]
-void rand_init_matrix(double** matrix,long int n)
+void rand_init_matrix(int** matrix,long int n)
 {
-	srand(time(NULL));
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			matrix[i][j] = 1 + rand() % 1000;
+			matrix[i][j] = 1 + rand()% 10;
 }
 
 // Matrix zeroing function
-void zero_init_matrix(double** matrix, long int n)
+void zero_init_matrix(int** matrix, long int n)
 {
 	srand(time(NULL));
 	for (int i = 0; i < n; i++)
@@ -44,8 +43,9 @@ void zero_init_matrix(double** matrix, long int n)
 
 int main(int argc, char* argv[])
 {
+	srand(time(NULL));
 	const long int N = atoi(argv[1]);
-	double** A, **B, **C;
+	int** A, **B, **C;
 	
 	// Memory allocation for matrices A, B, C
 	A = malloc_array(N);
@@ -56,15 +56,16 @@ int main(int argc, char* argv[])
 	rand_init_matrix(A, N);
 	rand_init_matrix(B, N);
 	
-	double** time_matrix = new double*[6];
-	for(int i = 0; i < 6; ++i)
+	
+	double** time_matrix = new double*[4];
+	for(int i = 0; i < 4; ++i)
 	{
 		time_matrix[i] = new double[10];
 	}
 
 	for (int threads = 1; threads <=10; threads++)
 	{
-		// Matrix multiplication with cycle order ijk
+		// Matrix multiplication with cycle order ijk omp
 		zero_init_matrix(C, N);
 		double time = omp_get_wtime();
 		#pragma omp parallel for num_threads(threads)
@@ -73,7 +74,8 @@ int main(int argc, char* argv[])
 				for (int k = 0; k < N; k++)
 					C[i][j] += A[i][k] * B[k][j];
 		time_matrix[0][threads - 1] = omp_get_wtime() - time;
-		// Matrix multiplication with cycle order ikj
+
+		// Matrix multiplication with cycle order ikj omp
 		zero_init_matrix(C, N);
 		time = omp_get_wtime();
 		#pragma omp parallel for num_threads(threads)
@@ -83,7 +85,7 @@ int main(int argc, char* argv[])
 					C[i][j] += A[i][k] * B[k][j];
 		time_matrix[1][threads - 1] = omp_get_wtime() - time;
 		
-		// Matrix multiplication with cycle order jik
+		// Matrix multiplication with cycle order jik omp
 		zero_init_matrix(C, N);
 		time = omp_get_wtime();
 		#pragma omp parallel for
@@ -92,8 +94,8 @@ int main(int argc, char* argv[])
 				for (int k = 0; k < N; k++)
 					C[i][j] += A[i][k] * B[k][j];
 		time_matrix[2][threads - 1] = omp_get_wtime() - time;
-
-		// Matrix multiplication with cycle order jki
+		
+		// Matrix multiplication with cycle order jki omp
 		zero_init_matrix(C, N);
 		time = omp_get_wtime();
 		#pragma omp parallel for
@@ -102,33 +104,49 @@ int main(int argc, char* argv[])
 				for (int i = 0; i < N; i++)
 					C[i][j] += A[i][k] * B[k][j];
 		time_matrix[3][threads - 1] = omp_get_wtime() - time;
-		
-		// Matrix multiplication with cycle order kij
-		zero_init_matrix(C, N);
-		time = omp_get_wtime();
-		#pragma omp parallel for
-		for (int k = 0; k < N; k++)
-			for (int i = 0; i < N; i++)
-				for (int j = 0; j < N; j++)
-					C[i][j] += A[i][k] * B[k][j];
-		time_matrix[4][threads - 1] = omp_get_wtime() - time;
-		
-		// Matrix multiplication with cycle order kji
-		zero_init_matrix(C, N);
-		time = omp_get_wtime();
-		#pragma omp parallel for
+	}
+	double* without_parallel = new double[4];
+	// Matrix multiplication with cycle order ijk
+	zero_init_matrix(C, N);
+	double time = omp_get_wtime();
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			for (int k = 0; k < N; k++)
+				C[i][j] += A[i][k] * B[k][j];
+	without_parallel[0] = omp_get_wtime() - time;
+
+	// Matrix multiplication with cycle order ikj
+	zero_init_matrix(C, N);
+	time = omp_get_wtime();
+	for (int i = 0; i < N; i++)
 		for (int k = 0; k < N; k++)
 			for (int j = 0; j < N; j++)
-				for (int i = 0; i < N; i++)
-					C[i][j] += A[i][k] * B[k][j];
-		time_matrix[5][threads - 1] = omp_get_wtime() - time;
-	}
+				C[i][j] += A[i][k] * B[k][j];
+	without_parallel[1] = omp_get_wtime() - time;
+		
+	// Matrix multiplication with cycle order jik
+	zero_init_matrix(C, N);
+	time = omp_get_wtime();
+	for (int j = 0; j < N; j++)
+		for (int i = 0; i < N; i++)
+			for (int k = 0; k < N; k++)
+				C[i][j] += A[i][k] * B[k][j];
+	without_parallel[2] = omp_get_wtime() - time;
+		
+	// Matrix multiplication with cycle order jki
+	zero_init_matrix(C, N);
+	time = omp_get_wtime();
+	for (int j = 0; j < N; j++)
+		for (int k = 0; k < N; k++)
+			for (int i = 0; i < N; i++)
+				C[i][j] += A[i][k] * B[k][j];
+	without_parallel[3] = omp_get_wtime() - time;	
 	
-	cout << "--------------------------------------------time matrix----------------------------------------" << endl; 
-	cout << "      1        2        3        4        5        6        7        8        9        10       " << endl; 
-	cout << "-----------------------------------------------------------------------------------------------" << endl; 
-	const char *row[6] = {"ijk", "ikj", "jik", "jki", "kij", "kji"};
-    for (int i = 0; i < 6; i++) 
+	cout << "-----------------------------------------------time matrix----------------------------------------" << endl; 
+	cout << "           1        2        3        4        5        6        7        8        9        10       " << endl; 
+	cout << "--------------------------------------------------------------------------------------------------" << endl; 
+	const char *row[4] = {"ijk(omp)", "ikj(omp)", "jik(omp)", "jki(omp)"};
+    for (int i = 0; i < 4; i++) 
 	{
 		printf("%s | ", row[i]);
 		for (int j = 0; j < 10; j++) 
@@ -138,10 +156,10 @@ int main(int argc, char* argv[])
 		cout << endl;
     }
 	
-	cout << "\n------------------------------------------efficiency matrix----------------------------------" << endl; 
-	cout << "      1        2        3        4        5        6        7        8        9        10       " << endl; 
-	cout << "-----------------------------------------------------------------------------------------------" << endl; 
-    for (int i = 0; i < 6; i++) 
+	cout << "\n---------------------------------------------efficiency matrix----------------------------------" << endl; 
+	cout << "           1        2        3        4        5        6        7        8        9        10       " << endl; 
+	cout << "--------------------------------------------------------------------------------------------------" << endl; 
+    for (int i = 0; i < 4; i++) 
 	{
 		printf("%s | ", row[i]);
 		for (int j = 0; j < 10; j++) 
@@ -150,6 +168,52 @@ int main(int argc, char* argv[])
 		}
 		cout << endl;
     }
+	const char *row_parallel[4] = {"ijk(not)", "ikj(not)", "jik(not)", "jki(not)"};
+	cout << "\n---------------------------------------------without parallel----------------------------------" << endl;
+	for (int i = 0; i < 4; i++) 
+	{
+		printf("%s, %f", row_parallel[i], without_parallel[i]);
+		cout << endl;
+	}
+	
+	bool output = false;
+	
+	if (output)
+	{
+		cout << "\nMatrix A" << endl; 
+		for (int i = 0; i < N; i++) 
+		{
+			for (int j = 0; j < N; j++) 
+			{
+				printf("%d ", A[i][j]);
+			}
+			cout << endl;
+		}
+	
+		cout << "\nMatrix B" << endl; 
+		for (int i = 0; i < N; i++) 
+		{
+			for (int j = 0; j < N; j++) 
+			{
+				printf("%d ", B[i][j]);
+			}
+			cout << endl;
+		}
+	
+		cout << "\nMatrix C" << endl; 
+		for (int i = 0; i < N; i++) 
+		{
+			for (int j = 0; j < N; j++) 
+			{
+				printf("%d ", C[i][j]);
+			}
+			cout << endl;
+		}
+	}
+	
+	
+	
+
 
 	// Freeing memory occupied by matrices A, B, C
 	free_array(A, N);

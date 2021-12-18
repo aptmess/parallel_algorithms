9bf64cb903894d046dc9449d0c621f2c3dc503b3
aptmess <improfeo@yandex.ru>
2021-12-18 19:54:20 +0300

    fix final task4

--- HT/hw_mpi/Assignment4.c
+++ HT/hw_mpi/Assignment4.c
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	MPI_Init(&argc, &argv);
 	int rank, n, i;
-	int message[4];
+	int message[5];
 	MPI_Status status;
 	MPI_Comm_size(MPI_COMM_WORLD, &n);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -16,8 +16,8 @@ int main(int argc, char* argv[]) {
 		for (i = 1; i < n; i++) 
 		{
 
-			MPI_Recv(&message[0], 4, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			cout << "Print numbers from process " << i << ": "; 
+			MPI_Recv(&message[0], 5, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			cout << "Print numbers from process " << message[4] << ": "; 
 			for (int j = 0; j < 4; j++)
 			{
 				cout << message[j] << ' ';
@@ -28,13 +28,14 @@ int main(int argc, char* argv[]) {
 	else 
 	{
 		srand(time(NULL) + rank);
-		int sending[4];
+		int sending[5];
 		for (int i = 0; i < 4; i++)
 		{
 			sending[i] = 1 + rand() % 20;
 		}
+		sending[4] = rank;
 
-		MPI_Send(&sending[0], 4, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(&sending[0], 5, MPI_INT, 0, 0, MPI_COMM_WORLD);
 	}
 	MPI_Finalize();
 	return 0;

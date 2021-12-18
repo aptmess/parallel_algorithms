a4d71fab2de11d454d5df3b65830efcdc19d964e
aptmess <improfeo@yandex.ru>
2021-12-18 19:00:43 +0300

    add assignment 4 solution

--- HT/hw_mpi/Assignment4.c
+++ HT/hw_mpi/Assignment4.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
 #include "mpi.h"
+#include <ctime>
+using namespace std;
+
 int main(int argc, char* argv[]) {
 	MPI_Init(&argc, &argv);
-	int rank, n, i, message;
+	int rank, n, i;
+	int message[4];
 	MPI_Status status;
 	MPI_Comm_size(MPI_COMM_WORLD, &n);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	if (rank == 0)
 	{
-		cout << "Hello from process " << rank << "\n";
-		for (i = 1; i < n; i++) {
-			MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE,
-			         MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			cout << "Hello from process " << message << endl;
+		cout << "Start main process " << rank << endl;
+		for (i = 1; i < n; i++) 
+		{
+			MPI_Recv(&message[0], 4, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			cout << "Print numbers from thread " << i << ": "; 
+			for (int j = 0; j < 4; j++)
+			{
+				cout << message[j] << ' ';
+			}
+			cout << endl;
 		}
 	}
-	else MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	else 
+	{
+		srand(time(NULL) + rank);
+		int sending[4];
+		for (int i = 0; i < 4; i++)
+		{
+			sending[i] = 1 + rand() % 20;
+		}
+
+		MPI_Send(&sending[0], 4, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
 	MPI_Finalize();
 	return 0;
 }

4dcc57928b836171fb004e38a876d7159f41ed80
aptmess <improfeo@yandex.ru>
2021-12-22 00:20:45 +0300

    add task 20

--- HT/hw_mpi/Assignment20.c
+++ HT/hw_mpi/Assignment20.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
+#include <iostream>
 #include "mpi.h"
 #define BUFSIZE 100
+
+using namespace std;
+
+
 int main(int argc, char **argv)
 {
 	int bufsize, num, sum;
+	
 	MPI_Init(&argc, &argv);
+	
 	MPI_Status status;
 	MPI_File fh;
+
 	char buf[BUFSIZE];
-	MPI_File_open(MPI_COMM_WORLD, "file.txt", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+	const char* filename = "file.txt";
+	string message ("hello my dear friend!");
+
+	int len = message.length();
+	const char *cstr = message.c_str();
+	
+	// start of writing to file "file.txt" with text "Hello world!"
+	MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+	MPI_File_write(fh, cstr, len, MPI_CHAR, &status);
+	MPI_File_close(&fh);
+	// end of writing to file
+
+	MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
 	MPI_File_set_view(fh, 0, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
 	sum = 0;
 	do {
@@ -19,5 +39,10 @@ int main(int argc, char **argv)
 	} while (num >= BUFSIZE);
 	MPI_File_close(&fh);
 	//print the number of read symbols sum from the file
+	printf("number of read symbols %d\n", sum);
+	if ((argc > 1) && (string(argv[1]) == "--delete-file"))
+	{
+		MPI_File_delete(filename, MPI_INFO_NULL);
+	}
 	MPI_Finalize();
 }

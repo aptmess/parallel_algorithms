d44a2d2ff1bb3f1f4c031068e3b6b23a39e2c894
aptmess <improfeo@yandex.ru>
2021-12-21 12:03:24 +0300

    add assignment 16

--- HT/hw_mpi/Assignment16.c
+++ HT/hw_mpi/Assignment16.c
@@ -1,14 +1,27 @@
-#include "mpi.h"
+#include <mpi.h>
+#include <stdio.h>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char **argv)
 {
+	int rank_split = atoi(argv[1]);
 	int rank, size, rank1;
 	MPI_Init(&argc, &argv);
 	MPI_Comm comm_revs;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_split(MPI_COMM_WORLD, 1, size - rank, &comm_revs);
+	if (rank == 0) cout << "rank % " << rank_split << '\n' << endl;
+	MPI_Comm_split(MPI_COMM_WORLD, rank % rank_split, size - rank, &comm_revs);
 	MPI_Comm_rank(comm_revs, &rank1);
+
 	//Display rank and rank1
+
+	cout << "rank=" << rank << ", rank1=" << rank1 << " group=" << rank % rank_split << endl;
+
 	MPI_Comm_free(&comm_revs);
 	MPI_Finalize();
 }

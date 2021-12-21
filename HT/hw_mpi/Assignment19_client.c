4b94fd086753d6a41e6c6ccd5d86f3b8cb46cf15
aptmess <improfeo@yandex.ru>
2021-12-21 23:19:54 +0300

    add task 19

--- HT/hw_mpi/Assignment19_client.c
+++ HT/hw_mpi/Assignment19_client.c
@@ -3,14 +3,22 @@
 #include "mpi.h"
 int main(int argc, char **argv)
 {
-	int rank;
+	int got, send = 42;
 	MPI_Init(&argc, &argv);
 	char port_name[MPI_MAX_PORT_NAME];
+	MPI_Status status;
 	MPI_Comm intercomm;
 	strcpy(port_name, argv[1]);
+	printf("Attempt to connect \n");
 	MPI_Comm_connect(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Send(&rank, 1, MPI_INT, 0, 0, intercomm);
+	printf("Server connection \n");
+	
+	MPI_Send(&send, 1, MPI_INT, 0, 0, intercomm);
+	printf("Client sent value: %d\n", send);
+
+	MPI_Recv(&got, 1, MPI_INT, 0, 0, intercomm, &status);
+	printf("Client got value: %d\n", got);
+
 	MPI_Finalize();
 	return 0;
 }
--- HT/hw_mpi/Assignment19_serv.c
+++ HT/hw_mpi/Assignment19_serv.c
@@ -2,18 +2,22 @@
 #include "mpi.h"
 int main(int argc, char **argv)
 {
-	int r;
+	int got, send = 25;
 	MPI_Init(&argc, &argv);
 	char port_name[MPI_MAX_PORT_NAME];
 	MPI_Status status;
 	MPI_Comm intercomm;
 	MPI_Open_port(MPI_INFO_NULL, port_name);
 	printf("portname: %s\n", port_name);
+	printf("Waiting for the client: \n");
 	MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
-	MPI_Recv(&r, 1, MPI_INT, 0, 0, intercomm, &status);
+	printf("Client connected \n");
+	MPI_Recv(&got, 1, MPI_INT, 0, 0, intercomm, &status);
+	MPI_Send(&send, 1, MPI_INT, 0, 0, intercomm);
 	MPI_Comm_free(&intercomm);
 	MPI_Close_port(port_name);
-	printf("Клиент отправил %d\n", r);
+	printf("Server sent value: %d\n", send);
+	printf("Server got value: %d\n", got);
 	MPI_Finalize();
 	return 0;
 }
